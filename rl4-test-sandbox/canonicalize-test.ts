50490464fb2bab9167f9209345cb7fa55113a51c
Soynido <vgaludec@gmail.com>
2025-12-04 17:53:31 +0100

    Add canonicalization test for AutoTasks detection

---
+++ rl4-test-sandbox/canonicalize-test.ts
@@ -0,0 +1,46 @@
+// Test file for canonicalization AutoTask detection
+
+// Standardized naming conventions (camelCase for variables)
+const myVariable = 1;
+const myOtherVariable = 2;
+const yetAnotherOne = 3;
+const someValue = 4;
+
+// Standardized function naming (camelCase)
+function doSomething() {
+  return true;
+}
+
+function doAnotherThing() {
+  return false;
+}
+
+function yetAnotherFunction() {
+  return null;
+}
+
+// Standardized class naming (PascalCase)
+class MyClass {
+  constructor() {}
+}
+
+class AnotherClass {
+  constructor() {}
+}
+
+// Standardized export styles (inline exports)
+export const value1 = 10;
+export const value2 = 20;
+
+// Standardized spacing and formatting
+const obj = { prop1: 1, prop2: 2, prop3: 3 };
+const arr = [1, 2, 3, 4, 5];
+
+// Standardized quotes (single quotes for strings)
+const str1 = 'double quotes';
+const str2 = 'single quotes';
+const str3 = `template literal`;
+
+// Export at the end
+export default MyClass;
+
